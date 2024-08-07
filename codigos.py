6eedd927d07f4e2a710ba99bfa0229cf88d43150
DevMGG <geraldimatheus07@gmail.com>
2024-08-07 11:55:56 -0300

    Add files via upload

---
+++ codigos.py
@@ -0,0 +1,5 @@
+n = float (input ("Digite a primeira nota: "))
+n2 = float (input ("Digite a segunda nota: "))
+n3 = float (input ("Digite a terceira nota: "))
+m = (n + n2 + n3) / 3
+print ("A sua média semestral é " '%.1f' %m)
